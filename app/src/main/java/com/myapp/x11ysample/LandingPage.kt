2825d1b71b5464eca98bbf1ae5812acf4cbfe7df
theapache64 <theapache64@gmail.com>
2023-12-08 23:13:55 +0530

    ◀️ repro ready

--- app/src/main/java/com/myapp/x11ysample/LandingPage.kt
+++ app/src/main/java/com/myapp/x11ysample/LandingPage.kt
@@ -1,6 +1,7 @@
 package com.myapp.x11ysample
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -18,10 +19,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
@@ -97,25 +100,29 @@ fun HorizontalMovies(
         )
         LazyRow {
             items(20) { index ->
-                Column(
-                    modifier = Modifier
-                        .padding(horizontal = 5.dp)
-                        .clickable {
-                            println("QuickTag: :HorizontalMovies: hello")
-                        }
-                ) {
-                    Box(
+                Box {
+                    Column(
                         modifier = Modifier
-                            .width(120.dp)
-                            .height(170.dp)
-                            .background(Purple, shape = RoundedCornerShape(4.dp))
-                            .clearAndSetSemantics { },
-                        contentAlignment = Alignment.Center,
+                            .testTag("some_tag_$index")
+                            .width(100.dp)
+                            .height(150.dp)
+                            .clip(shape = RoundedCornerShape(4.dp))
                     ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                            modifier = Modifier
+                                .clickable(
+                                    role = null
+                                ) {
+
+                                },
+                            contentScale = ContentScale.FillBounds,
+                            contentDescription = "dog photo",
+                            alignment = Alignment.Center,
+                        )
+
                         Text(text = "MOVIE $trayIndex-$index")
                     }
-
-                    Text(text = "MOVIE $trayIndex-$index")
                 }
             }
         }
